0d3a41b7fac8510c88a0aec3e27c1368a889115c
Vishal Saxena <vishal.saxena@fliplearn.com>
2019-12-09 12:22:52 +0530

    adding user model in django admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,13 +22,14 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create new super user"""
-        user.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
+        user.save(using=self._db)
 
         return user
-        
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
